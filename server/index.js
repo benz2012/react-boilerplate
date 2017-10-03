456f2de04da16cd230da3910a5e9ca2d42152ee9
Ben Zenker <imbenzenker@gmail.com>
2017-10-02 22:46:59 -0400

    adding Hot Module Replacement middleware for express

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
 const express = require('express')
 
+const hotMiddleware = require('./hot')
 
 // Globals
 const app = express()
 
 
+// Move to sparate server file, load only in development
+if (process.env.NODE_ENV !== 'production') {
+  hotMiddleware(app)
+}
+
+
 // Routes
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(`${process.cwd()}/public`))
 
 
 // Start Server
 const port = process.env.PORT || 8080
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
+  console.log(`\n\nserver listening on port ${port}\n`)
 })
